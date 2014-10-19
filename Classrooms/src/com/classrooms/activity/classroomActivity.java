31248f66324ea325e429860f0fce080d62e26eed
blitzkriegg <deblitzkriegg@gmail.com>
2014-10-20 00:21:05 +0800

    nav drawer prototype
    icon as drawer activator not working
    only works in classrooms page

--- Classrooms/src/com/classrooms/activity/classroomActivity.java
+++ Classrooms/src/com/classrooms/activity/classroomActivity.java
@@ -1,13 +1,18 @@
 package com.classrooms.activity;
 
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
 import com.classrooms.R;
 import com.classrooms.adapter.classroomTabListener;
 import com.classrooms.fragments.*;
@@ -16,29 +21,65 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.Color;
-import 	android.view.View;
-import android.view.LayoutInflater;
+
 
 public class classroomActivity extends Activity{
 	ActionBar.Tab tab1, tab2, tab3;
 	Fragment classroomHomeFragment = new classroomHomeFragment();
 	Fragment classroomPostsFragment = new classroomPostsFragment();
 	Fragment classroomFilesFragment = new classroomFilesFragment();
+	String[] menu;
+    DrawerLayout dLayout;
+    ListView dList;
+    ArrayAdapter<String> adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.classroom);
         Color color;
-
-
+        
+        menu = getResources().getStringArray(R.array.draweritems);
+        dLayout = (DrawerLayout) findViewById(R.id.classroom_drawer_layout);
+        dList = (ListView) findViewById(R.id.classroom_drawer);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu);
+        dList.setAdapter(adapter);
+        dList.setSelector(android.R.color.holo_blue_dark);
+        dList.setOnItemClickListener(new OnItemClickListener(){
+	        @Override
+	        public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+		        dLayout.closeDrawers();
+		        
+		        switch (position) {
+			    case 0:
+			    	Intent i = new Intent(getApplicationContext(),com.classrooms.activity.home.class);
+	                startActivity(i);
+			    	break;
+			    case 1:
+			    	Intent j = new Intent(getApplicationContext(),com.classrooms.activity.profile.class);
+	                startActivity(j);
+			      	break;
+			    case 2:
+			    	Intent k = new Intent(getApplicationContext(),com.classrooms.activity.makeClassroom.class);
+	                startActivity(k);
+			      	break;
+			    case 3:
+			    	Intent l = new Intent(getApplicationContext(),com.classrooms.activity.makePost.class);
+	                startActivity(l);
+			      	break;
+			    default:
+			    	break;
+		    }
+		        
+	        }
+        });
+        
+                
         ActionBar actionBar = getActionBar();
-
         //actionBar.setBackgroundDrawable(new ColorDrawable(Color.rgb(255,255,255))); "quicksand_book.otf"
 
         SpannableString s = new SpannableString("Classrooms");
@@ -46,11 +87,7 @@ public class classroomActivity extends Activity{
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         // Update the action bar title with the TypefaceSpan instance
-
         actionBar.setTitle(s);
-
-
-
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         
         tab1 = actionBar.newTab().setText("Home");
@@ -91,38 +128,4 @@ public class classroomActivity extends Activity{
 		classroomHomeFragment fragment = new classroomHomeFragment(); //  object of next fragment
 		fragment.setArguments(extras);
     }
-    
-    @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// TODO Auto-generated method stub
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-    
-    @Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
-		switch (item.getItemId()) {
-		    case R.id.menu_home:
-		    	Intent i = new Intent(this,com.classrooms.activity.home.class);
-                startActivity(i);
-		    	break;
-		    case R.id.menu_profile:
-		    	Intent j = new Intent(this,com.classrooms.activity.profile.class);
-                startActivity(j);
-		      	break;
-		    case R.id.menu_makeClassroom:
-		    	Intent k = new Intent(this,com.classrooms.activity.makeClassroom.class);
-                startActivity(k);
-		      	break;
-		    case R.id.menu_writePost:
-		    	Intent l = new Intent(this,com.classrooms.activity.makePost.class);
-                startActivity(l);
-		      	break;
-		    default:
-		    	break;
-	    }
-
-	    return true;
-	}
 }
--- Classrooms/src/com/classrooms/activity/makePost.java
+++ Classrooms/src/com/classrooms/activity/makePost.java
@@ -1,8 +1,10 @@
 package com.classrooms.activity;
 
 import android.app.ActionBar;
+import android.support.v4.widget.DrawerLayout;
 import android.text.Spannable;
 import android.text.SpannableString;
+
 import com.classrooms.R;
 
 import android.app.Activity;
@@ -12,17 +14,58 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class makePost extends Activity implements OnItemSelectedListener {
-
+	String[] menu;
+    DrawerLayout dLayout;
+    ListView dList;
+    ArrayAdapter<String> adapter;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.makepost);
+		
+		/*menu = getResources().getStringArray(R.array.draweritems);
+        dLayout = (DrawerLayout) findViewById(R.id.makepost_drawer_layout);
+        dList = (ListView) findViewById(R.id.makepost_drawer);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu);
+        dList.setAdapter(adapter);
+        dList.setSelector(android.R.color.holo_blue_dark);
+        dList.setOnItemClickListener(new OnItemClickListener(){
+	        @Override
+	        public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+		        dLayout.closeDrawers();
+		        
+		        switch (position) {
+			    case 0:
+			    	Intent i = new Intent(getApplicationContext(),com.classrooms.activity.home.class);
+	                startActivity(i);
+			    	break;
+			    case 1:
+			    	Intent j = new Intent(getApplicationContext(),com.classrooms.activity.profile.class);
+	                startActivity(j);
+			      	break;
+			    case 2:
+			    	Intent k = new Intent(getApplicationContext(),com.classrooms.activity.makeClassroom.class);
+	                startActivity(k);
+			      	break;
+			    case 3:
+			    	Intent l = new Intent(getApplicationContext(),com.classrooms.activity.makePost.class);
+	                startActivity(l);
+			      	break;
+			    default:
+			    	break;
+		    }
+		        
+	        }
+        });*/
 
         ActionBar actionBar =getActionBar();
 
