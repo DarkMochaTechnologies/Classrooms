dd6e0de533e0b087054afa773d3f491001ea8c47
blitzkriegg <michael.ymbong@gmail.com>
2014-10-23 17:10:12 +0800

    added select profpic to register

--- Classrooms/src/com/classrooms/activity/register.java
+++ Classrooms/src/com/classrooms/activity/register.java
@@ -1,20 +1,26 @@
 package com.classrooms.activity;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.text.Spannable;
 import android.text.SpannableString;
+
 import com.classrooms.R;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class register extends Activity{
-
+	private static int RESULT_LOAD_IMAGE = 1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -34,17 +40,30 @@ public class register extends Activity{
         typeFace[5] = Typeface.createFromAsset(getAssets(), "fonts/quicksand_light.otf");
         typeFace[6] = Typeface.createFromAsset(getAssets(), "fonts/quicksand_light_oblique.otf");
 		
+        Button buttonLoadImage = (Button) findViewById(R.id.register_button_selectpic);
 		Button register = (Button) findViewById(R.id.register_button_register);
 		final EditText username = (EditText)findViewById(R.id.register_editText_username);
 		final EditText email = (EditText)findViewById(R.id.register_editText_email);
 		final EditText password = (EditText)findViewById(R.id.register_editText_password);
 
-
+		buttonLoadImage.setTypeface(typeFace[5]);
         register.setTypeface(typeFace[5]);
         username.setTypeface(typeFace[6]);
         password.setTypeface(typeFace[6]);
         email.setTypeface(typeFace[6]);
 		
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                 
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                 
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
 		register.setOnClickListener(new View.OnClickListener() {
 			 
             public void onClick(View v) {
@@ -58,5 +77,25 @@ public class register extends Activity{
         
         });
 	}
-
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+         
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+ 
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+ 
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+             
+            ImageView imageView = (ImageView) findViewById(R.id.register_imageView_profilepic);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+         
+        }   
+    }
 }
